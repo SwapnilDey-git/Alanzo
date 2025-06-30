606a92d0813c48b9ae2ba35949ed54af6e45bfcc
Swapnil Dey <137487949+Swapnil-github006@users.noreply.github.com>
2025-06-30 10:41:14 +0530

    Initial commit

---
+++ Assests/js/app.js
@@ -0,0 +1,84 @@
+// ITEM SLIDER SECTION
+
+$(document).ready(function () {
+    $('.item-slider').slick({
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
+
+
+
+
+// FEEDBACK SLIDER
+
+// ITEM SLIDER SECTION
+
+$(document).ready(function () {
+    $('.feedback-slider').slick({
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        autoplay: true,
+        arrows: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: false
+                }
+            }
+        ]
+    });
+});
